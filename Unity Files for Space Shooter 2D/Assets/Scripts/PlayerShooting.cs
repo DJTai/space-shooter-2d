4728d61148c70663b00d3ffdfa4b792c4cf969b3
David T <djtai@protonmail.com>
2018-11-08 20:58:46 -0800

    add PlayerShooting.cs (not finished)

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public float fireDelay = 0.25f;
+	float cooldownTimer = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+		cooldownTimer -= Time.deltaTime;
+
+		if (Input.GetButton ("Fire1") && cooldownTimer <= 0) {
+			// Shoot!
+			cooldownTimer = fireDelay;
+
+
+		}
+			
+	}
+}
