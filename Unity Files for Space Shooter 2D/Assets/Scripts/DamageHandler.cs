b164ae0cca0c7d397ef104b4adee6b3c9f0881e4
David T <djtai@protonmail.com>
2018-11-08 20:03:47 -0800

    Add DamageHandler.cs

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour {
+
+	public int health = 1;
+	float invulnTimer = 0;
+	int correctLayer;
+
+	void Start() {
+		correctLayer = gameObject.layer;
+	}
+
+	void OnTriggerEnter2D() {
+		Debug.Log ("Trigger!");
+
+		health--;
+		invulnTimer = 0.50f;
+		gameObject.layer = 10;
+	}
+
+	void Update() {
+
+		invulnTimer -= Time.deltaTime;
+
+		if (invulnTimer <= 0) {
+			gameObject.layer = correctLayer;
+		}
+
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+}
