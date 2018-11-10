b446036fbd67109abb2e7546bbd7413db62ee40c
David T <djtai@protonmail.com>
2018-11-09 22:00:21 -0800

    Update enemy prefab and shooting script

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+
+	public GameObject bullePrefab;
+	Vector3 bulletOffset = new Vector3 (0, 0.5f, 0);
+
+	public float fireDelay = 0.75f;
+	float cooldownTimer = 0;
+
+	// Update is called once per frame
+	void Update () {
+
+		cooldownTimer -= Time.deltaTime;
+
+		if (cooldownTimer <= 0) {
+			// Shoot!
+			cooldownTimer = fireDelay;
+
+			Vector3 offset = transform.rotation * bulletOffset;
+
+			GameObject bulletGO = (GameObject)Instantiate (bullePrefab, transform.position + offset, transform.rotation);
+			bulletGO.layer = gameObject.layer;
+		}
+
+	}
+}
