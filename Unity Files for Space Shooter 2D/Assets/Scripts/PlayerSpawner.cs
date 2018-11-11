3c6c0fcfe235c6c92d9fe50dbdd7236e2109c810
David T <djtai@protonmail.com>
2018-11-10 19:47:07 -0800

    Minor changes to GUI re: player lives remaining

--- Unity Files for Space Shooter 2D/Assets/Scripts/PlayerSpawner.cs
+++ Unity Files for Space Shooter 2D/Assets/Scripts/PlayerSpawner.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerSpawner : MonoBehaviour {
 
 	public GameObject playerPrefab;
-
 	GameObject playerInstance;
+
+	public int numLives = 4;
 
 	float respawnTimer;
 
@@ -16,18 +17,27 @@ public class PlayerSpawner : MonoBehaviour {
 	}
 
 	void SpawnPlayer() {
+		numLives--;
 		respawnTimer = 1f;
 		playerInstance = (GameObject)Instantiate (playerPrefab, transform.position, Quaternion.identity);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (playerInstance == null) {
+		if (playerInstance == null && numLives > 0) {
 			respawnTimer -= Time.deltaTime;
 
 			if (respawnTimer <= 0) {
 				SpawnPlayer ();
 			}
+		}
+	}
+
+	void OnGUI() {
+		if (numLives > 0 || playerInstance != null) {
+			GUI.Label (new Rect (0, 0, 150, 50), "Remaining Lives: " + numLives);
+		} else {
+			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "GAME OVER");
 		}
 	}
 }
