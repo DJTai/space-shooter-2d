c4139ae325e134151faca87e879a9cb61cb6e042
David T <djtai@protonmail.com>
2018-11-10 19:09:14 -0800

    Created PlayerSpawner.cs
    Added script to respawn the user when starting and after the user has died in game

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public GameObject playerPrefab;
+
+	GameObject playerInstance;
+
+	float respawnTimer;
+
+	// Use this for initialization
+	void Start () {
+		SpawnPlayer ();
+	}
+
+	void SpawnPlayer() {
+		respawnTimer = 1f;
+		playerInstance = (GameObject)Instantiate (playerPrefab, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerInstance == null) {
+			respawnTimer -= Time.deltaTime;
+
+			if (respawnTimer <= 0) {
+				SpawnPlayer ();
+			}
+		}
+	}
+}
