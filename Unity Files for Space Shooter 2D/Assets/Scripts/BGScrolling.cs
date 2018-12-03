00c7125a91352f21b5d755b90960efd07f4cd204
Bill <billbui97@gmail.com>
2018-12-02 16:29:01 -0800

    Added Backgrounsd to Levels

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/BGScrolling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScrolling : MonoBehaviour {
+
+    public float moveSpeed;
+    Vector3 startPOS;
+    // Use this for initialization
+    void Start()
+    {
+
+        startPOS = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate((new Vector3(1, 0, 0)) * moveSpeed * Time.deltaTime);
+
+        
+        if (transform.position.y < -30.87697)
+        {
+            transform.position = startPOS;
+        }
+
+    }
+
+}
