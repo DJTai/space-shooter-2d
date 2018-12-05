20bcc9a13d0c62bb1eb1c4f6a8599b3d26b123c3
dangkelly4 <dangkelly4@gmail.com>
2018-12-05 13:39:45 -0800

    added logo

---
+++ Unity Files for Space Shooter 2D/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+
+    public bool paused;
+
+    // Use this for initialization
+    void Start() {
+        paused = false;
+    }
+
+    public void pauseGame()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
